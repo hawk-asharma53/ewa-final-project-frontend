b8fffc2f50e37acba6361e74fc82004be32f983b
biz-cloud-abhishek <103125284+biz-cloud-abhishek@users.noreply.github.com>
2022-10-28 10:50:06 -0500

    Checkout done

--- src/components/CheckoutPage/CheckoutPage.js
+++ src/components/CheckoutPage/CheckoutPage.js
@@ -11,13 +11,18 @@ const CheckoutPage = () => {
   const [value5, setValue5] = useState('');
 
   return (
-    <div className="grid m-8">
+    <div className="grid mx-8 mt-4">
+      <div className="col-12 mb-4">
+        <div className="flex flex-row justify-content-center">
+          <h1>Checkout</h1>
+        </div>
+      </div>
       <div className="col-8">
         <div className="col-12">
           <div className="grid">
-						<div className='col-12 mb-2'>
-							<h4>Your Informaion</h4>
-						</div>
+            <div className="col-12 mb-2">
+              <h4>Your Informaion</h4>
+            </div>
             <div className="col-4">
               <span className="p-float-label">
                 <InputText
@@ -53,12 +58,12 @@ const CheckoutPage = () => {
             </div>
           </div>
         </div>
-				<div className="col-12">
+        <div className="col-12">
           <div className="grid">
-						<div className='col-12 mb-2'>
-							<h4>Billing Informaion</h4>
-						</div>
-						<div className="col-6 mb-2">
+            <div className="col-12 mb-2">
+              <h4>Billing Informaion</h4>
+            </div>
+            <div className="col-6 mb-2">
               <span className="p-float-label">
                 <InputText
                   id="address1"
@@ -69,7 +74,7 @@ const CheckoutPage = () => {
                 <label htmlFor="address1">Address 1</label>
               </span>
             </div>
-						<div className="col-6">
+            <div className="col-6">
               <span className="p-float-label">
                 <InputText
                   id="address2"
@@ -115,19 +120,52 @@ const CheckoutPage = () => {
             </div>
           </div>
         </div>
-				<div className="col-12">
+        <div className="col-12">
           <div className="grid">
-						<div className='col-12 mb-2'>
-							<h4>Payment Informaion</h4>
-						</div>
+            <div className="col-12 mb-2">
+              <h4>Payment Informaion</h4>
+            </div>
+            <div className="col-6 w-12">
+              <span className="p-float-label">
+                <InputText
+                  id="creditCard"
+                  value={value2}
+                  onChange={e => setValue2(e.target.value)}
+                  className="w-12"
+                />
+                <label htmlFor="creditCard">Credit Card</label>
+              </span>
+            </div>
+            <div className="col-3 w-12">
+              <span className="p-float-label">
+                <InputText
+                  id="cvv"
+                  value={value2}
+                  onChange={e => setValue2(e.target.value)}
+                  className="w-12"
+                />
+                <label htmlFor="cvv">CVV</label>
+              </span>
+            </div>
+            <div className="col-3 w-12">
+              <span className="p-float-label">
+                <InputText
+                  id="expiry"
+                  value={value2}
+                  onChange={e => setValue2(e.target.value)}
+                  className="w-12"
+                />
+                <label htmlFor="expiry">Expiry</label>
+              </span>
+            </div>
           </div>
         </div>
       </div>
       <div className="col-4 summary-section">
         <div className="grid">
-					<div className="col-12">
-          	<h4>Summary</h4>
-					</div>
+          <div className="col-12">
+            <h4>Summary</h4>
+          </div>
           <div className="col-12">
             <div className="flex flex-row justify-content-between">
               <p>Subtotal</p>
@@ -141,6 +179,14 @@ const CheckoutPage = () => {
               <p className="text-black font-semibold">Total</p>
               <p className="text-black font-semibold">$299</p>
             </div>
+          </div>
+          <div className="col-12 seperator ml-2 mt-0"></div>
+          <div className="col-12 mt-2">
+            <h4>Items</h4>
+          </div>
+          <div className='col-12'>
+            <div>$299</div>
+            <p>1 X Woodworking craft</p>
           </div>
         </div>
       </div>
