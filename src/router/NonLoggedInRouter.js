2b1ee15e99eaf336e71c1299ec0e2d7f16e51de4
biz-cloud-abhishek <103125284+biz-cloud-abhishek@users.noreply.github.com>
2022-10-28 20:04:25 -0500

    Map functioality implemented

--- src/router/NonLoggedInRouter.js
+++ src/router/NonLoggedInRouter.js
@@ -9,6 +9,7 @@ import ServicesPage from '../components/ServicesPage/ServicesPage';
 import CheckoutPage from '../components/CheckoutPage/CheckoutPage';
 import { routes } from '../utility/constants';
 import Dashboard from 'components/Dashboard/Dashboard';
+import StoreLocationPage from 'components/StoreLocationPage/StoreLocationPage';
 
 export default () => {
   return (
@@ -21,6 +22,7 @@ export default () => {
       <Route exact path={routes.ABOUT} component={HomePage} />
       <Route exact path={routes.CHECKOUT} component={CheckoutPage} />
       <Route exact path={routes.DASHBOARD} component={Dashboard} />
+      <Route exact path={routes.MAP} component={StoreLocationPage} />
       <Route exact path="*" component={() => <Redirect to={routes.ROOT} />} />
     </Switch>
   );
